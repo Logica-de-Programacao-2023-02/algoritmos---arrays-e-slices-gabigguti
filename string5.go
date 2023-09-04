4b098a754fc1df5e4a92ef6e9d58f64cc7931c2d
gabigguti <119536472+gabigguti@users.noreply.github.com>
2023-09-04 10:41:14 -0300

    Update string5.go

--- string5.go
+++ string5.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	var matriz [3][4]int
-
-}
+	for linha := range matriz {
+		for coluna := range matriz[linha] {
+			matriz[linha][coluna] = linha + coluna
+		}
+	}
+	for _, l := range matriz {
+		fmt.Println(l)
+	}
+}	
